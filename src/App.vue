27d87992ce321d5a785379ed0e21b9568004b306
ismail akca <extismailak@d-teknoloji.com.tr>
2024-01-11 23:35:01 +0300

    feat:added navbar,font-family and bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  {{ name }}
+  <NavBar />
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
 export default {
   name: "App",
   data() {
@@ -10,5 +11,17 @@ export default {
       name: "ismail akca",
     };
   },
+  components: {
+    NavBar,
+  },
 };
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;600;700&display=swap");
+
+html,
+body {
+  font-family: "Mulish", sans-serif;
+}
+</style>
