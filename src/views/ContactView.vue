5f196381496b02fa00bfb7932fa4865e5ec126ff
ismail akca <extismailak@d-teknoloji.com.tr>
2024-01-17 21:53:13 +0300

    feat:Added section header component

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,14 @@
 <template lang="">
-  <div>ContactView Page</div>
+  <div class="container">
+    <SectionHeader title="Contact US" text="Contact Us Text" />
+  </div>
 </template>
 <script>
 export default {
   name: "ContactView",
+  components: {
+    SectionHeader,
+  },
 };
 </script>
 <style lang=""></style>
