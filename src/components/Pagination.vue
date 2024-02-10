3c58780e7599665e707f9843a05892e2bbc955ed
ismail akca <extismailak@d-teknoloji.com.tr>
2024-02-10 23:26:25 +0300

    started pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
   <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-end">
-      <li class="page-item">
+      <li class="page-item" :class="{ disabled: currentPage === 1 }">
         <a class="page-link" href="#" aria-label="Previous">
           <span aria-hidden="true">&laquo;</span>
         </a>
@@ -9,7 +9,7 @@
       <li class="page-item"><a class="page-link" href="#">1</a></li>
       <li class="page-item"><a class="page-link" href="#">2</a></li>
       <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
+      <li class="page-item" :class="{ disabled: currentPage === totalPages }">
         <a class="page-link" href="#" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
         </a>
